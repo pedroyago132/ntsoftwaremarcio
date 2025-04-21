616d4ad8da8cc0053699241b4354cdcffbf48e84
pepedroyago132 <pedroyago1324@gmail.com>
2025-04-21 15:17:33 -0300

    up

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -104,7 +104,7 @@ const Home = () => {
                         <FormControl color='primary'  >
                             <Logo src='/logont.jpg' alt='id' />
                             <Title>
-                               Sistema Web de Abina Silva
+                               Sistema Web de Abina 
                             </Title>
                             <TextField
                                 id="outlined-multiline-flexible"
